a070ca2036741b2bfd800c0f034b76a08351afe3
luanraithz <luanraithz@gmail.com>
2018-09-19 23:19:38 -0300

    adding cond

---
+++ src/Cond.rkt
@@ -0,0 +1,29 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Cond) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+(define I1 (rectangle 10 20 "solid" "red"))
+(define I2 (rectangle 20 20 "solid" "red"))
+(define I3 (rectangle 20 10 "solid" "red"))
+
+;; Image -> String
+;; produce shape of image, one of "tall", "square" or "wide"
+(check-expect (aspect-ratio I1) "tall")
+(check-expect (aspect-ratio I2) "square")
+(check-expect (aspect-ratio I3) "wide")
+
+;(define (aspect-ratio img) "")  ;stub
+
+;(define (aspect-ratio img)      ;template
+;  (... img))
+
+(define (aspect-ratio img)
+  (cond [(> (image-height img) (image-width img)) "tall"]
+        [(= (image-height img) (image-width img)) "square"]
+        [else "wide"]))
+
+;; cond expression:
+; (cond [Expression Result]
+;;      [Expression Result]
+;;      [else defaultResult]  
