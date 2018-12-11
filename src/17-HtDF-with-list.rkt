f86543f49c2487b2265ced4fe1e0473cf16bd1e8
luanraithz <luanraithz@gmail.com>
2018-12-11 07:32:30 -0200

    adding functions on list

--- src/17-HtDF-with-list.rkt
+++ src/17-HtDF-with-list.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname 16-List) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 17-HtDF-with-list) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
 ; Problem:
 
@@ -28,18 +28,41 @@ team-list
 (define LOS2 (cons "McGill" empty))
 (define LOS3 (cons "UBC" (cons "McGill" empty)))
 
-(define (fn-for-los los)
-  (cond [(empty? los) (...)]
-        [else  
-          (... (first lost) ; String
-               (fn-for-los (rest lost)) ; cons String ListOfString
-          )
-        ]
-  )
-)
+;(define (fn-for-los los)
+;  (cond [(empty? los) (...)]
+;        [else  
+;          (... (first lost) ; String
+;               (fn-for-los (rest lost)) ; cons String ListOfString
+;          )
+;        ]
+;  )
+;)
 
 ;; Template rules used:
 ;; - one of: 2 cases
 ;; - atomic distinct: empty
 ;; - compound: (cons "String" ListOfString)
+
+;; ListOfString -> Boolean
+;; produce true if los includes "UBC"
+
+; (define (contains-ubc? los) false)
+
+(define (contains-ubc? los)
+  (cond [(empty? los) false]
+        [else  
+          (if (string=? (first los) "UBC") true 
+               (contains-ubc? (rest los))
+          )
+        ]
+  )
+)
+
+
+(check-expect (contains-ubc? empty) false)
+(check-expect (contains-ubc? LOS3) true)
+(check-expect (contains-ubc? LOS2) false)
+(check-expect (contains-ubc? (cons "McGill" (cons "UBC" empty))) true)
+(check-expect (contains-ubc? (cons "McGill" (cons "Some other team's name" (cons "UBC" empty)))) true)
+(check-expect (contains-ubc? (cons "McGill" (cons "Some other team's name" (cons "And another one" empty)))) false)
 
