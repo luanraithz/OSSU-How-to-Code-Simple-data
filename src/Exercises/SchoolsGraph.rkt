f835173114cc77b74618e393639809a41bf79873
luanraithz <luanraithz@gmail.com>
2018-12-15 15:54:35 -0200

    implementing grahp's image

--- src/Exercises/SchoolsGraph.rkt
+++ src/Exercises/SchoolsGraph.rkt
@@ -1,5 +1,7 @@
-
-;; tuition-graph-starter.rkt  (just the problem statements)
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname SchoolsGraph) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; tuition-graph-starter.rkt 
 
 ; 
 ; PROBLEM:
@@ -24,6 +26,8 @@
 ;     the school with the lowest international student tuition.
 ; 
 
+(require 2htdp/image)
+
 (define-struct school (name price))
 ;; Player is (make-school String Number)
 ;; interp. (make-school name price) is a school with name and price
@@ -32,7 +36,7 @@
 
 (define S2 (make-school "Wid" 20000))
 
-(define S3 (make-school "Other" 19500))
+(define S3 (make-school "Other" 15000))
 
 (define (fn-for-school p)
   (... (school-name p)       ;String
@@ -50,11 +54,11 @@
 (define LOS1 empty)
 (define LOS2 (cons S1 (cons S3 (cons S3 empty))))
 
-(define (fn-for-los los)
-  (cond [Q A]
-        [Q A]
-        [else (... (fn-for-school (first los))
-        (fn-for-lost (rest los))])))
+;(define (fn-for-los los)
+;  (cond [Q A]
+;        [Q A]
+;        [else (... (fn-for-school (first los))
+;        (fn-for-lost (rest los)))]))
 
 ;; Template rules used:
 ;; - one of: 2 cases
@@ -70,3 +74,31 @@
 (define BAR-WIDTH 30)
 (define BAR-COLOUR "lightblue")
 
+;; Functions:
+
+;; School -> Image
+
+(define (get-square-for-school school)
+  (overlay/align "center" "bottom"
+    (rotate 90 (text (school-name school) TEXT-SIZE TEXT-COLOUR))
+    (rectangle BAR-WIDTH (* (school-price school) Y-SCALE) "outline" "black")
+    (rectangle BAR-WIDTH (* (school-price school) Y-SCALE) "solid" BAR-COLOUR)
+    ))
+
+;; ListOfSchool -> Image
+;; Produces bar chart showing names and tuitions of consumed schools
+
+
+(check-expect (charts empty) (square 0 "solid" "white"))
+(check-expect (charts (cons S2 empty))
+           (beside (get-square-for-school S2) (square 0 "solid" "white")))
+
+(check-expect (charts (cons S2 (cons S3 empty)))
+           (beside/align "bottom" (get-square-for-school S2) (beside/align "bottom" (get-square-for-school S3) (square 0 "solid" "white"))))
+
+
+
+(define (charts los) (if (empty? los) (square 0 "solid" "white")
+                         (beside/align "bottom" (get-square-for-school (first los)) (charts (rest los)))))
+
+(charts (cons S1 (cons S2 (cons S3 empty))))
