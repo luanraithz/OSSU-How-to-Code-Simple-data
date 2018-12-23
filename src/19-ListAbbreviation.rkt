6e8be790ae3a422f1f14b363560ec00d5957b60b
luanraithz <luanraithz@gmail.com>
2018-12-23 18:09:31 -0200

    src/19-ListAbbreviation.rkt

---
+++ src/19-ListAbbreviation.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname 19-ListAbbreviation) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(define L1 (list "B" "C"))
+(define L2 (list "D" "E" "F"))
+
+; (cons "A" L1)
+; (list "A" L1) ;; List with A and a another list
+
+; (list L1 L2)
+
+;; getting Lists togeteer
+(append L1 L2)
